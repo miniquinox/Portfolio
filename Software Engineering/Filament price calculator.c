6e4d566843c8019fabdf6c524457dfa0f39da5a0
miniquinox <63688331+miniquinox@users.noreply.github.com>
2020-07-26 11:05:16 -0700

    Add files via upload

---
+++ Software Engineering/Filament price calculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+	float grams;
+	float price = 41; //Dollars
+	float weight = 1500; //Grams
+	int i = 1;
+
+	printf("HOW MANY GRAMS? \n\n");
+
+
+	while (1) {
+		printf("Weight: ");
+		scanf_s("%f", &grams);
+
+		float final_price = ((price / weight) * grams); //Dollars
+		printf("It would take $%.2f to print it!\n\n", final_price);
+		i++;
+	}
+	return 0;
+}
+
+//#include <stdio.h>
+//int main() {
+//	// printf() displays the string inside quotation
+//	printf("Hello, World!");
+//	return 0;
+//}
